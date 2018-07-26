4b825abaf5e3554d1387d3d03a0dbf3676c65e3a
Pedro Marthon <pedro_marthon@live.com>
2018-07-26 12:01:58 +0300

    Added LocalDBHandler class

--- Untitled_MS/MainWindow.xaml.cs
+++ Untitled_MS/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Untitled_MS.Resources.Api;
 
 namespace Untitled_MS
 {
